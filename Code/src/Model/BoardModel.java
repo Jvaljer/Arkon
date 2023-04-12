50f5e2d0f98ccc8ff50b3a0cc83b2c4849c2c8f8
Jvaljer <abbel.haol@gmail.com>
2023-04-12 12:20:12 +0200

    started to implement board's initialization

--- Code/src/Model/BoardModel.java
+++ Code/src/Model/BoardModel.java
@@ -1,6 +1,9 @@
 package Model;
 
 import java.util.*;
+
+import Types.SlotId;
+
 import java.awt.*;
 
 /**
@@ -15,7 +18,8 @@ public class BoardModel {
 	//board's grid
 	private ArrayList<ArrayList<SlotModel>> slot_grid;
 	//board's slots parameters
-	private ArrayList<Point> init_luminas_coords;
+	private ArrayList<Point> luminas_coords;
+	private ArrayList<Point> power_points;
 	//board's size parameters
 	private final static int lines = 9;
 	private final static int columns = 9;
@@ -24,6 +28,20 @@ public class BoardModel {
 		game = GM;
 		
 		slot_grid = new ArrayList<ArrayList<SlotModel>>();
-		init_luminas_coords = new ArrayList<Point>();
+		luminas_coords = new ArrayList<Point>();
+		
+		for(int i=0; i<lines; i++) {
+			slot_grid.add(new ArrayList<SlotModel>());
+			for(int j=0; j<columns; j++) {
+				slot_grid.get(i).add(new SlotModel(this, SetSlotIdFromCoord(i,j), new Point(i,j)));
+			}
+		}
+		
+		power_points = new ArrayList<Point>();
+	}
+	
+	public SlotId SetSlotIdFromCoord(int x, int y) {
+		//must implement
+		return null;
 	}
 }
