3a7209d44e23101002c9189132bfd0f18e510372
Jvaljer <abbel.haol@gmail.com>
2023-04-13 14:21:05 +0200

    modified token roles' implementation

---
+++ Code/src/Types/TokenRole.java
@@ -0,0 +1,32 @@
+package Types;
+
+public enum TokenRole {
+	//Dark roles
+	Goblin("Goblin",0),
+	Manticore("Manticore",0),
+	Basilisk("Basilisk",0),
+	Banshee("Banshee",0),
+	ShapeShifter("ShapeShifter",0),
+	Dragon("Dragon",0),
+	Troll("Troll",0),
+	Sorceress("Sorceress",0),
+	//Light roles
+	Knight("Knight",1),
+	Valkyrie("Valkyrie",1),
+	Archer("Archer",1),
+	Djinn("Djinn",1),
+	Golem("Golem",1),
+	Unicorn("Unicorn",1),
+	Phoenix("Phoenix",1),
+	Sorcerer("Sorcerer",1),
+	//exception
+	None("None",2);
+	
+	private final String name;
+	private final int side;
+	
+	private TokenRole(String str, int bin) {
+		name = str;
+		side = bin;
+	}
+}
